d4a43ca99a2f271ccb3dd2fc8ee212e8bd4e9c5e
Oleksandra Sherstiuk <you@example.com>
2021-10-13 20:43:09 +0300

    added registration and login

--- task2/controllers/authController.js
+++ task2/controllers/authController.js
@@ -1,10 +1,23 @@
 const User = require('../models/User')
 
 const bcrypt = require('bcryptjs')
+const {validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const {secret} = require('../config')
+
+const generateAccessToken = (id) => {
+    const payload = {id}
+
+    return jwt.sign(payload, secret, {expiresIn: "24h"})
+}
 
 class authController {
     async registration(request, response) {
         try {
+            const errors = validationResult(request)
+            if(!errors.isEmpty()) {
+                return response.status(400).json({message: 'Bad request'});
+            }
             const {username, password} = request.body;
 
             const hashedPassword = bcrypt.hashSync(password, 7)
@@ -19,9 +32,27 @@ class authController {
 
     async login(request, response) {
         try {
+            const {username, password} = request.body
 
+            const user = await User.findOne({username})
+            if(!user) {
+                return response.status(400).json({message: 'Bad request1'})
+            }
+
+            const validPassword = bcrypt.compareSync(password, user.password)
+            if(!validPassword) {
+                return response.status(400).json({message: 'Bad request2'})
+            }
+
+            const token = generateAccessToken(user._id)
+
+            return response.status(200).json({
+                message: "Success",
+                jwt_token: token
+            })
         } catch(e) {
-            response.status(400).json({message: 'Bad request'})
+            console.log(e)
+            response.status(400).json({message: 'Bad request3'})
         }
     }
 }
--- task2/routes/authRouter.js
+++ task2/routes/authRouter.js
@@ -1,9 +1,13 @@
 const Router = require('express')
 const router = new Router()
+const {check} = require('express-validator')
 
 const authController = require('../controllers/authController')
 
-router.post('/register', authController.registration)
+router.post('/register',[
+    check('username', 'Username cannot be empty').notEmpty(),
+    check('password', 'Password cannot be empty').notEmpty(),
+] , authController.registration)
 router.post('/login', authController.login)
 
 module.exports = router
